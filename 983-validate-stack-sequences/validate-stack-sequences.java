bd80941b5e3de17d6d0dec7d95bd5eb6523f6b00
vtu26134 <vtu26134@veltech.edu.in>
2026-02-13 11:19:41 +0530

    Time: 1 ms (94.74%) | Memory: 46.1 MB (94.90%) - LeetSync

---
+++ 983-validate-stack-sequences/validate-stack-sequences.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int j = 0;
+
+        for (int x : pushed) {
+            stack.push(x);
+            while (!stack.isEmpty() && stack.peek() == popped[j]) {
+                stack.pop();
+                j++;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
