252099c72230c57738da65e8315e8c8d3c4e86e7
vtu26134 <vtu26134@veltech.edu.in>
2026-02-13 11:18:06 +0530

    Time: 5 ms (83.04%) | Memory: 47 MB (83.64%) - LeetSync

---
+++ 155-min-stack/min-stack.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class MinStack {
+    private Deque<Integer> stack;
+    private Deque<Integer> minStack;
+
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        int val = stack.pop();
+        if (val == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
