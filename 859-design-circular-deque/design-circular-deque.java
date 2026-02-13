5d1401a24a9d2c06b5e92992f6abb804179df838
vtu26134 <vtu26134@veltech.edu.in>
2026-02-13 11:40:32 +0530

    Time: 5 ms (86.94%) | Memory: 46.8 MB (11.14%) - LeetSync

---
+++ 859-design-circular-deque/design-circular-deque.java
@@ -0,0 +1,63 @@
+class MyCircularDeque {
+    private int[] data;
+    private int front;
+    private int rear;
+    private int size;
+    private int capacity;
+
+    public MyCircularDeque(int k) {
+        data = new int[k];
+        front = 0;
+        rear = 0;
+        size = 0;
+        capacity = k;
+    }
+
+    public boolean insertFront(int value) {
+        if (isFull()) return false;
+        front = (front - 1 + capacity) % capacity;
+        data[front] = value;
+        size++;
+        return true;
+    }
+
+    public boolean insertLast(int value) {
+        if (isFull()) return false;
+        data[rear] = value;
+        rear = (rear + 1) % capacity;
+        size++;
+        return true;
+    }
+
+    public boolean deleteFront() {
+        if (isEmpty()) return false;
+        front = (front + 1) % capacity;
+        size--;
+        return true;
+    }
+
+    public boolean deleteLast() {
+        if (isEmpty()) return false;
+        rear = (rear - 1 + capacity) % capacity;
+        size--;
+        return true;
+    }
+
+    public int getFront() {
+        if (isEmpty()) return -1;
+        return data[front];
+    }
+
+    public int getRear() {
+        if (isEmpty()) return -1;
+        return data[(rear - 1 + capacity) % capacity];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == capacity;
+    }
+}
