2a773fb0fc7bd33e53dfa86bb8bada224c48e804
vtu26134 <vtu26134@veltech.edu.in>
2026-04-08 10:08:50 +0530

    Time: 5 ms (85.46%) | Memory: 47 MB (74.52%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,33 +1,40 @@
-import java.util.*;
-
 class MinStack {
-    private Deque<Integer> stack;
-    private Deque<Integer> minStack;
-
+    Stack<Integer> st;
+    Stack<Integer> minStack;
     public MinStack() {
-        stack = new ArrayDeque<>();
-        minStack = new ArrayDeque<>();
+        st=new Stack<>();
+        minStack=new Stack<>();
     }
-
+    
     public void push(int val) {
-        stack.push(val);
-        if (minStack.isEmpty() || val <= minStack.peek()) {
-            minStack.push(val);
+        st.push(val);
+        if(minStack.isEmpty())minStack.push(val);
+        else{
+            if(minStack.peek()>=val){
+                minStack.push(val);
+            }
         }
     }
-
+    
     public void pop() {
-        int val = stack.pop();
-        if (val == minStack.peek()) {
-            minStack.pop();
-        }
+        int val=st.pop();
+        if(val==minStack.peek())minStack.pop();
     }
-
+    
     public int top() {
-        return stack.peek();
+        return st.peek();
     }
-
+    
     public int getMin() {
         return minStack.peek();
     }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
